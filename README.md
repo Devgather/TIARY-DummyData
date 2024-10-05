df9b479bec6dd422806aa6d2fc5d3bfaa3a4ba33
Igoc <zx0342@gmail.com>
2024-10-05 20:36:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TIARY-DummyData
+
+TIARY 프로젝트를 위한 더미 데이터 생성기
---
+++ src/main/java/me/tiary/dummydata/TiaryDummyDataApplication.java
@@ -0,0 +1,11 @@
+package me.tiary.dummydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiaryDummyDataApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(TiaryDummyDataApplication.class, args);
+    }
+}
